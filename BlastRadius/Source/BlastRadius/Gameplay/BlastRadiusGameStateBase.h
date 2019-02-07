4bebf20d40bddb3e2a6f63658b4d1cbb7c2048ed
Remi Teeple <remi.teeple@gmail.com>
2019-02-07 14:45:44 -0500

    initial project re-oraganization additional classes created for later work

---
+++ BlastRadius/Source/BlastRadius/Gameplay/BlastRadiusGameStateBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "BlastRadiusGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTRADIUS_API ABlastRadiusGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ BlastRadius/Source/BlastRadius/Weapon/BlastRadiusWeapon.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BlastRadiusWeapon.h"
+
+
+// Sets default values
+ABlastRadiusWeapon::ABlastRadiusWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABlastRadiusWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABlastRadiusWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
